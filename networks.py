ae4e2c780c1d3c7eb7353d4af0ebe52a16116383
TheSpaceSheep <noe.malais@gmail.com>
2022-03-25 15:04:37 +0100

    finish actor network

--- networks.py
+++ networks.py
@@ -11,15 +11,10 @@ Critic, Value and Actor networks
 # Inherits from MLP so no need to reimplement forward/__call__
 class CustomMLP(hk.nets.MLP):
     """
-    General MLP class for checkpoint saving/loading
-    Value, Critic and Actor networks inherit this class
+    General MLP class for checkpoint saving/loading.
+    Value, Critic and Actor networks inherit this class.
 
-    obs_dims: int
-            observation space number of dimensions
-    actions_dims: int
-            action space number of dimensions
-
-    takes an input of size obs_dims+action_dims
+    takes an input of size self._expected_input_dims
     """
     def __init__(self, output_sizes: Sequence[int],
                  non_linearity:str='relu',
@@ -41,11 +36,12 @@ class CustomMLP(hk.nets.MLP):
 
         self.chkpt_dir = chkpt_dir
         self._chkpt_file = None  # will be set when saving
-        self._input_dims = None       # should be set in child classes
+        self._expected_input_dims = None       # should be set in child classes
 
-   def assert_input_dim(x: chex.Array) -> None
-       assert x.shape[1] == self._input_dims
-       "input dimension {x.shape} doesn't match expected dimension [batch_size, {self._input_dims}]"
+    def __call__(x: chex.Array) -> chex.Array:
+        assert x.shape[1] == self._expected_input_dims,
+        "input dimension {x.shape} doesn't match expected dimension [batch_size, {self._expected_input_dims}]"
+        return super().__call__(x)
 
     def save_checkpoint(self, file='value_net'):
         """call with a unique filename, otherwise previous file will be overwrited"""
@@ -64,45 +60,52 @@ class CustomMLP(hk.nets.MLP):
 
 class CriticNetwork(CustomMLP):
     def __init__(self, obs_dims: int, action_dims: int,
+                 hidden_output_dims=(256, 256),
                  non_linearity: str = 'relu'
                  chkpt_dir: str = None):
 
         super().__init__(
-            output_sizes=(256, 256, 1), non_linearity=non_linearity, chkpt_dir=chkpt_dir
+            output_sizes=(*hidden_output_dims, 1),
+            non_linearity=non_linearity,
+            chkpt_dir=chkpt_dir
         )
 
-        self._input_dims = obs_dims+action_dims  # to assert correct input dims
+        self._expected_input_dims = obs_dims+action_dims  # to assert correct input dims
 
-    def __call__(self, state_action_pair: chex.Array) -> Array
-        self.assert_input_dim(state_action_pair)
-        return super().__call__(state_action_pair)
 
 class ValueNetwork(CustomMLP):
     def __init__(self, obs_dims: int,
+                 hidden_output_dims=(256, 256),
                  non_linearity: str = 'relu'
                  chkpt_dir: str = None):
 
         super().__init__(
-            output_sizes=(256, 256, 1), non_linearity=non_linearity, chkpt_dir=chkpt_dir
+            output_sizes=(*hidden_output_dims, 1),
+            non_linearity=non_linearity,
+            chkpt_dir=chkpt_dir
         )
 
-        self._input_dims = obs_dims  # to assert correct input dims
+        self._expected_input_dims = obs_dims  # to assert correct input dims
 
-    def __call__(self, state: chex.Array) -> Array
-        self.assert_input_dim(state)
-        return super().__call__(state)
 
 class ActorNetwork(CustomMLP):
-    def __init__(self, obs_dims, action_dims,
-                non_linearity='relu',
-                chkpt_dir=None)
+    def __init__(self, action_dims,
+                 hidden_output_dims=(256, 256),
+                 non_linearity='relu',
+                 chkpt_dir=None)
         super().__init__(
-            output_sizes=(256, 256, 1), non_linearity=non_linearity, chkpt_dir=chkpt_dir
+            output_sizes=(*hidden_output_dims, 2*action_dims),
+            non_linearity=non_linearity,
+            chkpt_dir=chkpt_dir
         )
 
     self.input_dims = obs_dims
     self.reparam_noise = 1e-6
 
-    def __call__(x: chex.Array) -> chex.Array:
-        # handle sampling from distribution
-        pass
+    def __call__(state: chex.Array) -> chex.Array:
+        h = super().__call__(state)
+        mu, log_sigma = jnp.split(h, 2, axis=-1)
+        log_sigma = jax.nn.softplus(log_sigma)
+        # log_sigma = jnp.clip(log_sigma, min=jnp.log(self.reparam_noise))
+        return mu, log_sigma  # need to reshape that ?
+
