91c3712819c959bce75089e9542b203289f6b564
TheSpaceSheep <noe.malais@gmail.com>
2022-03-26 12:32:56 +0100

    implement action selection

--- agents.py
+++ agents.py
@@ -9,6 +9,8 @@ import chex
 from typing import Mapping
 from new_types import Trajectory
 
+from networks import ValueNetwork, CriticNetwork, ActorNetwork
+
 # A very simple agent API, with just enough to interact with the environment
 # and to update its potential parameters.
 class Agent(object):
@@ -32,4 +34,66 @@ class Agent(object):
 
 
 class SACAgent(Agent):
-    pass
+    def __init__(self,
+                 rng,
+                 obs_spec,
+                 action_spec,
+                 lr=3e-4,  # same for all networks
+                 hidden_output_dims = (256, 256),
+                 gamma=0.99,
+                 tau=0.005, # target smoothing coefficient,
+                 target_update=1000,
+                 chkpt_dir=None):
+
+        obs_dims = obs_spec().shape[-1]
+        action_dims = action_spec().shape[-1]
+
+        self.value_target = ValueNetwork(obs_dims,
+                                         hidden_output_dims=hidden_output_dims,
+                                         chkpt_dir=chkpt_dir)
+        self.value = ValueNetwork(obs_dims,
+                                  hidden_output_dims=hidden_output_dims,
+                                  chkpt_dir=chkpt_dir)
+        self.critic = CriticNetwork(obs_dims,
+                                    action_dims,
+                                    hidden_output_dims=hidden_output_dims,
+                                    chkpt_dir=chkpt_dir)
+        self.actor = ActorNetwork(obs_dims,
+                                  action_dims,
+                                  hidden_output_dims=hidden_output_dims,
+                                  chkpt_dir=chkpt_dir)
+
+        self.rng = rng
+        self.obs_spec = obs_spec
+        self.action_spec = action_spec
+
+
+    def select_actions(observations: chex.Array) -> chex.Array:
+        """
+        observations: chex.Array
+                batch of states
+
+        for each observations, generates action distribution from actor
+        and samples from this distribution
+        also computes log_probs, to compute loss function
+        return: actions, log_probs
+        """
+        rng, key = jax.random.split(rng, 2)
+        mus, log_sigmas = self.actor(observations)
+        # sample actions according to normal distributions
+        actions = jax.random.multivariate_normal(key, mus, jnp.diag(jnp.exp(sigmas)))
+
+        # compute log_likelihood of the sampled actions
+        log_probs = -0.5*jnp.log(2*jnp.pi) - log_sigmas - ((actions-mus)/(2*jnp.exp(sigmas)))**2
+
+        # squash actions to enforce action bounds
+        actions = jnp.tanh(actions) * (self.action_spec().maximum() - self.action_spec().minimum())
+
+        # compute squashed log-likelihood
+        # ! other implementations put a relu in the log
+        # + 1e-6 to prevent log(0)
+        log_probs -= jnp.sum(jnp.log(1 - jnp.tanh(actions)**2 + 1e-6), axis=1)
+
+
+        return actions, log_probs
+
--- networks.py
+++ networks.py
@@ -38,33 +38,34 @@ class CustomMLP(hk.nets.MLP):
                 os.mkdir(chkpt_dir)
 
         self.chkpt_dir = chkpt_dir
-        self._chkpt_file = None  # will be set when saving
+        self.chkpt_file = None           # will be set when saving
 
-        self._expected_input_dims = None       # should be set in child classes
+        self._expected_input_dims = None  # should be set in child classes
 
     def __call__(x: chex.Array) -> chex.Array:
         assert x.shape[1] == self._expected_input_dims,
         "input dimension {x.shape} doesn't match expected dimension [batch_size, {self._expected_input_dims}]"
         return super().__call__(x)
 
-    def save_checkpoint(self, file='value_net'):
+    def save_checkpoint(self, file=None):
         """call with a unique filename, otherwise previous file will be overwrited"""
         logger.info(f'Saving value network to {self.chkpt_dir}/{file}')
-        self._chkpt_file = file
-        with open(os.path.join(self.chkpt_dir, file), 'wb') as f:
+        if file is not None:
+            self.chkpt_file = file
+        with open(os.path.join(self.chkpt_dir, self.chkpt_file), 'wb') as f:
             pickle.dump(self.params_dict(), f)
 
     def load_checkpoint(self, file=None):
         """loads last saved checkpoint by default"""
         logger.info(f'Loading value network from {self.chkpt_dir}/{file}')
-        if file is None:
-            file = self._chkpt_file
+        if file is not None:
+            self.chkpt_file = file
 
-        if not os.path.exists(os.path.join(self.chkpt_dir, file)):
+        if not os.path.exists(os.path.join(self.chkpt_dir, self.chkpt_file)):
             logger.warning('could not load checkpoint file (file not found)')
             return None
 
-        with open(os.path.join(self.chkpt_dir, file), 'rb') as f:
+        with open(os.path.join(self.chkpt_dir, self.chkpt_file), 'rb') as f:
             self.params_dict() = pickle.load(f)
 
 
@@ -81,6 +82,7 @@ class CriticNetwork(CustomMLP):
 
         self._expected_input_dims = obs_dims+action_dims  # to assert correct input dims
 
+
 class ValueNetwork(CustomMLP):
     def __init__(self, obs_dims: int,
                  hidden_output_dims=(256, 256),
@@ -96,7 +98,7 @@ class ValueNetwork(CustomMLP):
 
 
 class ActorNetwork(CustomMLP):
-    def __init__(self, obs_dims, action_dims,
+    def __init__(self, obs_dims: int, action_dims: int,
                  hidden_output_dims=(256, 256),
                  non_linearity='relu',
                  chkpt_dir=None)
@@ -113,7 +115,11 @@ class ActorNetwork(CustomMLP):
     def __call__(state: chex.Array) -> chex.Array:
         h = super().__call__(state)
         mu, log_sigma = jnp.split(h, 2, axis=-1)
+
+        # TODO: these next are present in other implementations, 
+        # but i don't see where they are explained in the paper ?
+
         log_sigma = jax.nn.softplus(log_sigma)
-        # log_sigma = jnp.clip(log_sigma, min=jnp.log(self.reparam_noise))
+        # log_sigma = jnp.clip(log_sigma, min=jnp.log(self.reparam_noise))  # prevent -inf values
         return mu, log_sigma  # need to reshape that ?
 
